02a7524371a433709c1da157fe0688fe12e3fbef
thomas brown <thomasbrown9101@gmail.com>
2019-07-01 21:14:47 -0700

    finsihed up to step  10

---
+++ server/controllers/books_controller.js
@@ -0,0 +1,40 @@
+let books = [];
+let id = 0;
+
+
+
+module.exports = {
+    read: (req,res, next) => {
+        res.status(200).send(books)
+    },
+    create: (req,res, next) => {
+        const { title, author } = req.body;
+        let book = {
+            id: id,
+            title: title,
+            author: author
+        }
+        books.push(book)
+        id++
+        res.status(200).send(books);
+    },
+    update: (req, res, next) => {
+        const { author, title } = req.body
+        const updateID = req.params.id;
+        const bookIndex = books.findIndex(book => book.id == updateID);
+        let book = books[bookindex];
+
+        books[bookIndex] = {
+            id: book.id,
+            title: title || book.title,
+            author: author || book.author
+        },
+        res.status(200).send(books);
+     },
+     delete: (req, res, next) => {
+         const deleteID = req.params.id;
+         bookIndex = books.findIndex(book => book.id == deleteID);
+         books.splice(bookIndex, 1);
+         res.status(200).send(books)
+     }
+}
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const bc = require("./controllers/books_controller.js");
+
+
+app.get("/api/books", bc.read);
+
+app.post("/api/books", bc.create);
+
+app.put("/api/books/:id", bc.update);
+
+app.delete("/api/books/:id", bc.delete);
+
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`SERVER LISTENING ON PORT ${port}`)
+})
